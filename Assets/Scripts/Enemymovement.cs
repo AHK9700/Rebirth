125829e753a691e56466d578f866fbf0b2f41fa6
GCM-SchuRich <66574825+GCM-SchuRich@users.noreply.github.com>
2020-06-20 12:21:38 -0700

    Update rpg

---
+++ Assets/Scripts/Enemymovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float LookRadius = 10f;
+    //The target is the player
+    Transform target;
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //you need a refrence in the player manger to the player
+        target = PlayerManger.instance.player.transform;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Distance = Vector3.Distance(target.position, transform.position);
+
+        if (Distance <= LookRadius)
+
+        {
+            agent.SetDestination(target.position);
+        }
+
+        if (Distance <= agent.stoppingDistance) 
+        {
+            //Attack target 
+            FaceTarget();
+        }
+    }
+    //This is about facing the target(player)
+    void FaceTarget () 
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion LookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, LookRotation, Time.deltaTime * 5f);
+    }
+
+    //This is about the lookRadius
+    void OnGizmosSleceted ()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius );
+ 
+    }
+}
---
+++ Assets/Scripts/PlayerManger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManger : MonoBehaviour
+{
+
+    public static PlayerManger instance;
+
+    void Awake ()     
+    {
+
+        instance = this;
+ 
+    }
+
+    public GameObject player;
+
+
+
+
+}
