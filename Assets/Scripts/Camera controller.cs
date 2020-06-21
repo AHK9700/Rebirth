d6cb5b79f96a918de8b3321275d17d3802bac533
AHK9700 <67010770+AHK9700@users.noreply.github.com>
2020-06-21 09:52:31 +0400

    Add files via upload

---
+++ Assets/Scripts/Camera controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour
+{
+
+    Transform player;
+    public Vector3 offset;
+
+    public float pitch = 3f;
+
+    public float Yawspeed = 100f;
+    private float YawInput = 0f;
+    
+
+    private float currentzoom = 10f;
+    public float zoomspeed = 4f;
+    public float minizoom = 5f;
+    public float maxzoom = 15f;
+
+
+    void Update()
+    {
+        currentzoom -= Input.GetAxis("Mouse ScrollWheel") * zoomspeed;
+        currentzoom = Mathf.Clamp(currentzoom, minizoom, maxzoom);
+        YawInput -= Input.GetAxis("Horizontal") * Yawspeed * Time.deltaTime;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = transform.position - offset * currentzoom;
+        transform.LookAt(player.position + Vector3.up * pitch);
+        transform.RotateAround(player.position, Vector3.up, YawInput);
+    }
+
+
+}
---
+++ Assets/Scripts/player controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(playerMotor))]
+public class playercontroller : MonoBehaviour
+{
+
+    public LayerMask movementMask;
+    Camera cam;
+    playerMotor motor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<playerMotor>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+
+                motor.MoveToPoint(hit.point);
+
+            }
+        }
+
+        
+    }
+}
